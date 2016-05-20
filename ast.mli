3cf9948bf555bc41ecc0bc0da6186f56e4df88a0
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2016-05-21 04:09:44 +0900

    fix

--- ast.mli
+++ ast.mli
@@ -1,4 +1,4 @@
-(* https://github.com/ignatov/intellij-erlang/blob/master/grammars/erlang.bnf *)
+(* https://github.com/ignatov/intellij-erlang/blob/mter/grammars/erlang.bnf *)
 
 type op = op_desc Located.t
 
@@ -36,64 +36,49 @@ type op_desc =
   | Op_lshift           (* "bsl" *)
   | Op_rshift           (* "bsr" *)
 
-type ast = ast_desc located.t
+type t = desc Located.t
 
-type ast_desc =
-  | Module_decl of ast list
-  | Module of text attr
-  | Export of type_spec list attr
-  | Export_type of type_spec list attr
-  | Import of type_spec list attr
-  | On_load of type_spec list attr
-  | Version of ast attr
-  | Author of text attr
+type desc =
+  | Module of (t, Location.t) Seplist.t
+  | Module_attr of module_attr
   | Fun_decl of fun_clause list
-  | Guard_funccall of string * ast
-  | Guard_bin of ast * bin_op * ast
-  | Guard_unary of ast * unary_op
-  | Guard_app of string * ast list
-  | Guard_record of ast option * string * string
-  | Guard_list of list_pat
-  | Guard_tuple of ast list
-  | Guard_paren_test of ast
-  | Guard_paren of ast
-  | Catch of ast
-  | Block of ast
-  | Body of ast list
-  | If of (ast * ast) list (* guard, clause body *)
-  | Case of ast * cr_clause list
-  | Receive of cr_clause list * (ast * ast) option
-  | Fun of ast
-  | Fun_arity of ast option * ast * int
-  | Query of ast
-  | Unexp of ast * unary_op
-  | Binexp of ast * bin_op * ast
-  | Fun_call of fun_call
-  | Fully_qual of ast * ast
+  | Catch of (Location.t * t)
+  | Block of exp_list enclosed
+  | If of (t * t) list (* guard, clause body *)
+  | Case of t * cr_clause list
+  | Receive of cr_clause list * (t * t) option
+  | Anon_fun of anon_fun
+  | Module_fun of module_fun
+  | Query of t
+  | Unexp of (Location.t * t)
+  | Binexp of binexp
+  | Call of fun_call
   | Qual of text * text (* atom '.' atom *)
-  | Record_creation of ast option * ast * ast
-  | Record_access of ast option * ast * ast list
-  | Paren of ast enclosed
-  | Var of string
-  | Universal (* "_" *)
-  | Atom of string
-  | Char of string
-  | String of string
-  | Int of int
-  | Float of float
-  | List_pat of list_pat
-  | Tuple of elt_list enclosed
-  | Record of ast * ast list (* record name, assocs *)
-  | Assoc of ast * ast (* field name, value *)
-  | List_comp of ast * ast list
-  | Generator of ast * ast (* pattern, _exp *)
+  | Record_creation of t option * t * t
+  | Record_access of t option * t * t list
+  | Paren of t enclosed
+  | Uscore
+  | Var of text
+  | Atom of text
+  | Q_atom of text enclosed
+  | Char of text
+  | String of text
+  | Int of text
+  | Float of text
+  | List_ptn of list_ptn
+  | Tuple of exp_list enclosed
+  | Record of t * t list (* record name, assocs *)
+  | Assoc of t * t (* field name, value *)
+  | List_compr of compr (* list comprehension *)
+  | Map of map
+  | Bits_compr of compr (* bitstring comprehension *)
 
-and 'a attr = {
-  attr_minus : Location.t;
-  attr_lparen : Location.t;
-  attr_rparen : Location.t;
-  attr_desc : 'a;
-  attr_dot : Location.t;
+and module_attr = {
+  mattr_minus : Location.t;
+  mattr_tag : text;
+  mattr_open : Location.t;
+  mattr_value : t;
+  mattr_close : Location.t;
 }
 
 and type_spec = {
@@ -102,27 +87,86 @@ and type_spec = {
 }
 
 and fun_clause = {
-  fun_clause_name : string option;
-  fun_clause_patterns : ast list;
-  fun_clause_guard : ast list;
-  fun_clause_body : ast;
+  fun_clause_name : text;
+  fun_clause_body : fun_body;
+  fun_clause_end : Location.t;
 }
 
-and fun_call = {
-  fun_call_mod : ast option;
-  fun_call_func : ast;
-  fun_call_args : ast list;
+and fun_body = {
+  fun_body_lparen: Location.t;
+  fun_body_ptnterns : t list;
+  fun_body_rparen: Location.t;
+  fun_body_guard : t list;
+  fun_body_body : t;
+}
+
+and anon_fun = {
+  anon_fun_begin : Location.t;
+  anon_fun_body : fun_body;
+  anon_fun_end : Location.t;
+}
+
+and module_fun = {
+  module_fun_prefix : Location.t;
+  module_fun_mname : text option;
+  module_fun_colon : Location.t;
+  module_fun_fname : text;
+  module_fun_slash : Location.t;
+  module_fun_arity : text;
+}
+
+and fun_name = {
+  fname_mname : t option;
+  fname_colon : Location.t option;
+  fname_fname : text;
+}
+
+and call = {
+  call_fname : fun_name;
+  call_open : Location.t;
+  call_args : exp_list;
+  call_close : Location.t;
 }
 
 and cr_clause = {
-  cr_clause_pattern : ast;
-  cr_clause_gaurd : ast list;
-  cr_clause_body : ast;
+  cr_clause_ptntern : t;
+  cr_clause_gaurd : t list;
+  cr_clause_body : t;
 }
 
-and list_pat = {
-  list_pat_head : ast list;
-  list_pat_tail : ast option;
+and binexp = {
+  binexp_op : Location.t;
+  binexp_left : t;
+  binexp_right : t;
+}
+
+and list_ptn = {
+  list_ptn_open : Location.t;
+  list_ptn_head : t list;
+  list_ptn_bar : Location.t;
+  list_ptn_tail : t option;
+  list_ptn_close : Location.t;
+}
+
+and compr = {
+  compr_open : Location.t;
+  compr_exp : t;
+  compr_sep : Location.t;
+  compr_quals : exp_list;
+  compr_close : Location.t;
+}
+
+and map = {
+  map_sharp : Location.t;
+  map_open : Location.t;
+  map_assocs : assoc Seplist.t;
+  map_close : Location.t;
+}
+
+and assoc = {
+  assoc_key : t;
+  assoc_val : t;
+  assoc_sep : Location.t;
 }
 
 and text = string Located.t
@@ -133,4 +177,4 @@ and 'a enclosed = {
   enc_close : Location.t;
 }
 
-and alt_list = (ast, Location.t) Seplist.t
+and exp_list = (t, Location.t) Seplist.t

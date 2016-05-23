d470e0465dd02d54858df87ef3a6ea2b4ac54967
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2016-05-23 22:25:43 +0900

    update

--- ast.mli
+++ ast.mli
@@ -39,23 +39,21 @@ type op_desc =
 type t = desc Located.t
 
 type desc =
-  | Module of (t, Location.t) Seplist.t
+  | Module of seq
   | Module_attr of module_attr
-  | Fun_decl of fun_clause list
-  | Catch of (Location.t * t)
-  | Block of exp_list enclosed
+  | Fun_decl of (fun_clause, token) Seplist.t
+  | Catch of (token * t)
+  | Block of seq enclosed
   | If of (t * t) list (* guard, clause body *)
-  | Case of t * cr_clause list
-  | Receive of cr_clause list * (t * t) option
+  | Case of case
+  | Receive of case_clause list * (t * t) option
   | Anon_fun of anon_fun
   | Module_fun of module_fun
   | Query of t
-  | Unexp of (Location.t * t)
+  | Unexp of (token * t)
   | Binexp of binexp
   | Call of fun_call
   | Qual of text * text (* atom '.' atom *)
-  | Record_creation of t option * t * t
-  | Record_access of t option * t * t list
   | Paren of t enclosed
   | Uscore
   | Var of text
@@ -66,115 +64,149 @@ type desc =
   | Int of text
   | Float of text
   | List_ptn of list_ptn
-  | Tuple of exp_list enclosed
-  | Record of t * t list (* record name, assocs *)
-  | Assoc of t * t (* field name, value *)
+  | Tuple of seq enclosed
+  | Record of record
+  | Field of field
+  | Update_record of t * record
   | List_compr of compr (* list comprehension *)
   | Map of map
+  | Update_map of update_map
   | Bits_compr of compr (* bitstring comprehension *)
 
 and module_attr = {
-  mattr_minus : Location.t;
+  mattr_minus : token;
   mattr_tag : text;
-  mattr_open : Location.t;
+  mattr_open : token;
   mattr_value : t;
-  mattr_close : Location.t;
-}
-
-and type_spec = {
-  type_name : string;
-  type_arity : int;
+  mattr_close : token;
 }
 
 and fun_clause = {
-  fun_clause_name : text;
-  fun_clause_body : fun_body;
-  fun_clause_end : Location.t;
+  fun_clause_name : text option;
+  fun_clause_open : token;
+  fun_clause_ptns : seq;
+  fun_clause_close : token;
+  fun_clause_when : token option;
+  fun_clause_guard : seq;
+  fun_clause_arrow : token;
+  fun_clause_body : seq;
 }
 
-and fun_body = {
-  fun_body_lparen: Location.t;
-  fun_body_ptnterns : t list;
-  fun_body_rparen: Location.t;
-  fun_body_guard : t list;
-  fun_body_body : t;
+and case = {
+  case_begin : token;
+  case_of : token;
+  case_clauses : (case_clause, token) Seplist.t;
+  case_end : token;
+}
+
+and case_clause = {
+  case_clause_ptn : t;
+  case_clause_close : token;
+  case_clause_when : token option;
+  case_clause_guard : seq;
+  case_clause_arrow : token;
+  case_clause_body : seq;
 }
 
 and anon_fun = {
-  anon_fun_begin : Location.t;
+  anon_fun_begin : token;
   anon_fun_body : fun_body;
-  anon_fun_end : Location.t;
+  anon_fun_end : token;
 }
 
 and module_fun = {
-  module_fun_prefix : Location.t;
+  module_fun_prefix : token;
   module_fun_mname : text option;
-  module_fun_colon : Location.t;
+  module_fun_colon : token;
   module_fun_fname : text;
-  module_fun_slash : Location.t;
+  module_fun_slash : token;
   module_fun_arity : text;
 }
 
 and fun_name = {
-  fname_mname : t option;
-  fname_colon : Location.t option;
-  fname_fname : text;
+  fun_name_mname : t option;
+  fun_name_colon : token option;
+  fun_name_fname : text;
 }
 
 and call = {
   call_fname : fun_name;
-  call_open : Location.t;
-  call_args : exp_list;
-  call_close : Location.t;
+  call_open : token;
+  call_args : seq;
+  call_close : token;
 }
 
-and cr_clause = {
-  cr_clause_ptntern : t;
-  cr_clause_gaurd : t list;
-  cr_clause_body : t;
+and case_clause = {
+  case_clause_ptntern : t;
+  case_clause_gaurd : t list;
+  case_clause_body : t;
 }
 
 and binexp = {
-  binexp_op : Location.t;
+  binexp_op : token;
   binexp_left : t;
   binexp_right : t;
 }
 
 and list_ptn = {
-  list_ptn_open : Location.t;
+  list_ptn_open : token;
   list_ptn_head : t list;
-  list_ptn_bar : Location.t;
+  list_ptn_bar : token;
   list_ptn_tail : t option;
-  list_ptn_close : Location.t;
+  list_ptn_close : token;
 }
 
 and compr = {
-  compr_open : Location.t;
+  compr_open : token;
   compr_exp : t;
-  compr_sep : Location.t;
-  compr_quals : exp_list;
-  compr_close : Location.t;
+  compr_sep : token;
+  compr_quals : seq;
+  compr_close : token;
+}
+
+and record = {
+  rec_sharp : token;
+  rec_name : text;
+  rec_open : token;
+  rec_assocs : (rec_assoc, token) Seplist.t;
+  rec_close : token;
+}
+
+and rec_assoc = {
+  rec_assoc_key : t;
+  rec_assoc_val : t;
+  rec_assoc_sep : token;
+}
+
+and field = {
+  field_exp : t;
+  field_sharp : token;
+  field_rname : text;
+  field_sep : token;
+  field_fname : text;
 }
 
 and map = {
-  map_sharp : Location.t;
-  map_open : Location.t;
-  map_assocs : assoc Seplist.t;
-  map_close : Location.t;
+  map_sharp : token;
+  map_open : token;
+  map_assocs : (map_assoc, token) Seplist.t;
+  map_close : token;
 }
 
-and assoc = {
-  assoc_key : t;
-  assoc_val : t;
-  assoc_sep : Location.t;
+and map_assoc = {
+  map_assoc_key : t;
+  map_assoc_val : t;
+  map_assoc_sep : [`Put | `Update] token;
 }
 
 and text = string Located.t
 
 and 'a enclosed = {
-  enc_open : Location.t;
+  enc_open : token;
   enc_desc : 'a;
-  enc_close : Location.t;
+  enc_close : token;
 }
 
-and exp_list = (t, Location.t) Seplist.t
+and seq = (t, token) Seplist.t
+
+and token = Location.t

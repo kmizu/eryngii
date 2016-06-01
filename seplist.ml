ead10212203394e0e870f7162d1f8b814b5aae6b
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2016-06-01 15:27:16 +0900

    impl seplist

--- seplist.ml
+++ seplist.ml
@@ -1,1 +1,38 @@
-type ('a, 'b) t = C of 'a * ('b, 'a) t
+type ('a, 'b) t =
+  | Cons of 'a * ('b, 'a) t
+  | Nil
+
+let empty = Nil
+
+let one e = Cons (e, Nil)
+
+let cons e ~sep es = Cons (e, Cons (sep, es))
+
+let rev es =
+  let rec f es accu =
+    match es with
+    | Nil -> Nil
+    | Cons (e, Nil) -> Cons (e, Nil)
+    | Cons (_e, Cons (_sep, Nil)) -> failwith "error"
+    | Cons (e, Cons (sep, es)) ->
+      f es @@ Cons (sep, Cons (e, accu))
+  in
+  f es Nil
+
+let fold_left es ~init ~f =
+  let rec fold_left' es accu =
+    match es with
+    | Nil -> accu
+    | Cons (e, Nil) -> f accu None e
+    | Cons (_e, Cons (_sep, Nil)) -> failwith "error"
+    | Cons (e, Cons (sep, es)) ->
+      fold_left' es @@ f accu (Some sep) e
+  in
+  fold_left' es init
+
+let length es =
+  fold_left es ~init:0 ~f:(fun accu _sep _e -> accu + 1)
+
+let iter es ~f =
+  fold_left es ~init:() ~f:(fun _accu sep e -> f sep e)
+
--- seplist.mli
+++ seplist.mli
@@ -1,2 +1,9 @@
 type ('a, 'b) t
 
+val empty : ('a, 'b) t
+val one : 'a -> ('a, 'b) t
+val cons : 'a -> sep:'b -> ('a, 'b) t -> ('a, 'b) t
+val rev : ('a, 'b) t -> ('a, 'b) t
+val length : ('a, 'b) t -> int
+val iter : ('a, 'b) t -> f:('b option -> 'a -> unit) -> unit
+val fold_left : ('a, 'b) t -> init:'c -> f:('c -> 'b option -> 'a -> 'c) -> 'c

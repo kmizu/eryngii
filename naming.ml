732818e75cc88ad808206729352a08156bd9e271
SUZUKI Tetsuya <szktty@shiguredo.jp>
2017-04-08 00:29:15 +0900

    uppercase

--- naming.ml
+++ naming.ml
@@ -17,11 +17,11 @@ let pre_case s i =
 let next_case s i =
   if i+1 >= String.length s then None else Some (case @@ String.get s (i+1))
 
-let lowercase s =
+let convert_case s ~f =
   String.foldi s
     ~init:[]
     ~f:(fun i accu c ->
-        let c' = Char.lowercase c in
+        let c' = f c in
         match pre_case s i, case c, next_case s i with
         | Some `Upper, `Upper, Some `Lower
         | Some `Lower, `Upper, _ ->
@@ -30,11 +30,19 @@ let lowercase s =
   |> List.rev
   |> String.of_char_list
 
+let lowercase s = convert_case s ~f:Char.lowercase
+
+let uppercase s = convert_case s ~f:Char.uppercase
+
 let test () =
-  Printf.printf "lowercase: %s\n" (lowercase "HTTP");
-  Printf.printf "lowercase: %s\n" (lowercase "HTTPClient");
-  Printf.printf "lowercase: %s\n" (lowercase "ClientA");
-  Printf.printf "lowercase: %s\n" (lowercase "CamelCaseName");
+  Printf.printf "lowercase: HTTP -> %s\n" (lowercase "HTTP");
+  Printf.printf "lowercase: HTTPClient -> %s\n" (lowercase "HTTPClient");
+  Printf.printf "lowercase: ClientA -> %s\n" (lowercase "ClientA");
+  Printf.printf "lowercase: CamelCaseName -> %s\n" (lowercase "CamelCaseName");
+  Printf.printf "uppercase: HTTP -> %s\n" (uppercase "HTTP");
+  Printf.printf "uppercase: HTTPClient -> %s\n" (uppercase "HTTPClient");
+  Printf.printf "uppercase: ClientA -> %s\n" (uppercase "ClientA");
+  Printf.printf "uppercase: CamelCaseName -> %s\n" (uppercase "CamelCaseName");
 ;;
 
-test ()
+(* test () *)
